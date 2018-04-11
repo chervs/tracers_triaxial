2133608899e3efb41f6a2891b330b04d52fca5ac
jngaravitoc <jngaravitoc@email.arizona.edu>
2018-04-11 14:52:19 -0700

    added function to compute the velocity dispersion of the tracers

--- Stracers/tracers_properties.py
+++ Stracers/tracers_properties.py
@@ -6,260 +6,204 @@ sys.path.append('../../MW_anisotropy/code/')
 from pygadgetreader import readsnap
 from .weights import *
 
-# TEMPORAL !!!!!!!!!!!!!!
-def all_host_particles(xyz, vxyz, pids, pot, mass, N_host_particles):
-    """
-    Function that return the host and the sat particles
-    positions and velocities.
-
-    Parameters:
-    -----------
-    xyz: snapshot coordinates with shape (n,3)
-    vxys: snapshot velocities with shape (n,3)
-    pids: particles ids
-    Nhost_particles: Number of host particles in the snapshot
-    Returns:
-    --------
-    xyz_mw, vxyz_mw, xyzlmc, vxyz_lmc: coordinates and velocities of
-    the host and the sat.
-
-    """
-    sort_indexes = np.sort(pids)
-    N_cut = sort_indexes[N_host_particles]
-    host_ids = np.where(pids<N_cut)[0]
-    return xyz[host_ids], vxyz[host_ids], pids[host_ids], pot[host_ids], mass[host_ids]
-
-def radial_bins(r, nbins):
-    r_hist, r_edges = np.histogram(r, nbins)
-    dr = r_edges[1]-r_edges[0]
-    r_edges += dr/2
-    return r_hist, r_edges[:-1]
 
 
-def finding_indices(ids_w, ids):
-    """
-    Return the indicies in which the ids of the two snapshots are the same.
+
+def den_profile(r, mass, rbins, rcut):
     """
 
-    indices = np.zeros(len(ids_w))
-    for i in range(len(indices)):
-        indices[i] = (np.where(ids=ids_w[i])[0])
-
-    return indices
-
-
-def Stellar_properties(ids_weights, weights, ids, mass, pos, vel):
-    """
-    Paramters:
-    ----------
-
-    ids_weights :
-
-    weights :
-
-    ids :
-
-    mass :
-
-    pos :
-
-    vel :
-
-    """
-    assert len(ids_weights)<= len(ids), 'Error: length of weights ids is larger than lenght of ids!'
-
-    common_ids = np.isin(ids, ids_weights)
-    # selecting elements that are common in weight_ids and ids:
-    ids_n = ids[common_ids]
-    mass_n = mass[common_ids]
-    pos_n = pos[common_ids]
-    vel_n = vel[common_ids]
-
-    assert len(ids_n) == len(ids_weights), 'Same number of particles'
-    # Sorting ids:
-    ids_w_arg_sort = np.argsort(ids_weights)
-    ids_arg_sort = np.argsort(ids_n)
-
-
-    comp_items = ids_weights[ids_w_arg_sort]==ids_n[ids_arg_sort]
-    false_ind = np.where(comp_items==False)[0]
-    assert (len(false_ind)==0), 'Error: Hey!'
-
-    # Sorting ids and properties
-    weights_n = weights[ids_w_arg_sort]
-    ids_weights_n = ids_weights[ids_w_arg_sort]
-
-    # Sorting future properties
-    ids_n = ids_n[ids_arg_sort]
-    mass_n = mass_n[ids_arg_sort]*weights_n
-    posx = pos_n[ids_arg_sort,0]
-    posy = pos_n[ids_arg_sort,1]
-    posz = pos_n[ids_arg_sort,2]
-
-    velx = vel_n[ids_arg_sort,0]*weights_n
-    vely = vel_n[ids_arg_sort,1]*weights_n
-    velz = vel_n[ids_arg_sort,2]*weights_n
-
-    all_pos = np.array([posx, posy, posz]).T
-    all_vel = np.array([velx, vely, velz]).T
-
-    return [ids_weights_n, weights_n, ids_n], mass_n, all_pos, all_vel
-
-def energies(snap, N_host_particles, rcut=0, lmc=0):
-    """
-    Paramters:
-    ----------
-
-    snap : string
-        path and name of the snapshot
-
-    rcut : int
-        truncation radii (no trunction by default rcut=0)
-
-
-    Returns:
-    --------
-    Distances (rr)
-    Kinetic energy (Ekk)
-    Potential (MW_pot)
-    Ids (MW_ids)
-    Mass (MW_mass)
-    Pos (MW_pos)
-    Vel (MW_vel)
-
-    """
-
-    pp= readsnap(snap, 'pos', 'dm')
-    vv= readsnap(snap, 'vel', 'dm')
-    massarr= readsnap(snap, 'mass', 'dm')
-    Epp = readsnap(snap, 'pot', 'dm')
-    ids = readsnap(snap, 'pid', 'dm')
-
-
-    # Selecting MW particles
-    #N_host_particles = 100000000
-    if lmc ==1 :
-        MW_pos, MW_vel, MW_ids, MW_pot, MW_mass = all_host_particles(pp, vv, ids, Epp, massarr, N_host_particles)
-    elif lmc==0:
-        MW_pos, MW_vel, MW_ids, MW_pot, MW_mass = pp, vv, ids, Epp, massarr
-
-    #assert len(MW_ids)==N_host_particles, 'Error: something went wrong selecting the host particles'
-
-    rr=np.sqrt(MW_pos[:,0]**2+MW_pos[:,1]**2+MW_pos[:,2]**2)
-
-
-    if rcut>0:
-        r_cut = np.where((rr<rcut))[0]
-
-        rr = rr[r_cut]
-        MW_pos = MW_pos[r_cut]
-        MW_vel = MW_vel[r_cut]
-        MW_mass = MW_mass[r_cut]
-        MW_pot = MW_pot[r_cut]
-        MW_ids = MW_ids[r_cut]
-
-    v2=MW_vel[:,0]**2+MW_vel[:,1]**2+MW_vel[:,2]**2
-    Ekk=0.5*v2
-
-    return rr, Ekk, MW_pot, MW_ids, MW_mass, MW_pos, MW_vel
-
-
-def stellar_quantity(weights, pids, wids, pq):
-    """
-    Assign the stellar particle weights to a given physical
-    quantity (pq).
-
-    Parameters:
-    ----------
-
-    Returns:
-    --------
-
-
-    """
-
-    assert len(pids)==len(wids) , 'Length of ids arrays should be the same'
-    assert len(weights)==len(wids) , 'Length of ids arrays should be the same'
-
-
-    sort_index_w=np.argsort(wids)
-    weights_sorted=weights[sort_index_w]
-
-    sort_index_ids = np.argsort(pids)
-    pq_sort = pq[sort_index_ids]
-
-    phys_quantitiy_w = pq_sort * weights_sorted
-
-    return phys_quantitiy_w, wids[sort_index_w]
-
-
-def den_tracers(w, wids, r, mass, nbins, rcut):
-    """
+    Computes the density profiles of a halo.
 
     Paramters:
     -----------
 
-    w : ndarray
-        Weights of the DM particles
-    wids: ndarray
-        Array with the ids of the weights particles, the firs id is missing (not yet clear why, but is in weight_triaxial function)
-    r : ndarray
+    r : numpy.array
         Array with the distances to the particles
 
-    mass : ndarray
+    mass : numpy.array
+        Masses of the particles.
     nbins : int
-    rcut : float
+        Number of radial bins to use.
+    rcut : Float
+        Radius at which truncate the halo.
 
 
 
 
     To-do:
-    1. Implement the mass!
-    2. Check the mass.
-    3. organize this function.
-    4. Implement assert conditions.
+    ------
+
     """
 
 
 
-    r = r[1:] # remove the first index to even len(ids)=len(wids)
-    #mass = mass[1:]
+    r_bins = np.linspace(0, 100, rbins)
+    dr = (r_bins[1] - r_bins[0])/2.0
+    rho_bins = np.zeros(rbins-1)
+    for i in range(1, len(r_bins)):
+       index = np.where((r<r_bins[i]) & (r>=r_bins[i-1]))[0]
+       V = 4/3. * np.pi * (r_bins[i]**3-r_bins[i-1]**3)
+       rho_bins[i-1] = np.sum(m[index]) / V
+
+    return r_bins+dr, density
 
 
-    s=np.argsort(wids)
-    weights=w[s]
-    r_ids = r[s]
+def vel_cartesian_to_spherical(pos, vel):
+    """
+    Computes velocities in spherical coordinates from cartesian.
 
-    w=np.where(ids>1) ## account for the fact there are a bounch of weights=zeros
-    r_ids = r_ids[s]
+    Parameters:
+    -----------
+
+    pos : numpy.array
+        3-D Cartesian positions of the particles.
+    vel : numpy.array
+        3-D Cartesian velocities of the particles.
+
+    Returns:
+    --------
+
+    vr :
+    vthehta :
+    vphi :
+
+    """
+    r = (pos[:,0]**2 + pos[:,1]**2 + pos[:,2]**2)**0.5
+    theta = np.arccos(pos[:,2]/r)
+    phi = np.arctan2(pos[:,1], pos[:,0])
+
+    vr = np.sin(theta)*np.cos(phi)*vel[:,0] \
+         +  np.sin(theta)*np.sin(phi)*vel[:,1] \
+         +  np.cos(theta)*vel[:,2]
+
+    v_theta = np.cos(theta)*np.cos(phi)*vel[:,0]\
+              + np.cos(theta)*np.sin(phi)*vel[:,1]\
+              - np.sin(theta)*vel[:,2]
+
+    v_phi = -np.sin(phi)*vel[:,0] + np.cos(phi)*vel[:,1]
+
+    return vr, v_theta, v_phi
+
+def velocity_dispersion(pos, vel):
+    """
+    Computes the velocity dispersions in spherical coordinates.
 
 
-    #mass_w = mass[s]*weights
+    Parameters:
+    ----------
+    pos : 3d numpy array
+        Array with the cartesian coordinates of the particles
+    vel : 3d numpy array
+        Array with the cartesian velocities of the particles
 
-    r_edges = np.linspace(1, rcut, nbins)
+    Returns:
+    --------
+    sigma_r : float
+        The value of sigma_r.
+    sigma_theta : float
+        The value of sigma_theta
+    sigma_phi : float
+        The value of sigma_phi
 
-    mass_bins=np.ndarray(shape=np.size(r_edges)-1, dtype=float)
+    """
+
+    vr, v_theta, v_phi = vel_cartesian_to_spherical(pos, vel)
+    sigma_r = np.std(vr)
+    sigma_theta = np.std(v_theta)
+    sigma_phi = np.std(v_phi)
+
+    return sigma_r, sigma_theta, sigma_phi
+
+def velocity_dispersion_weights(pos, vel, weights):
+    """
+    Computes the velocity dispersions for stellar particles using
+    the weights fro DM particles.
+
+    Uses Eq: (3) in Laporte 13a to compute the velocity dispersion this is:
+
+    \sigma_* =  \dfrac{\sum_{i}^N \omega_i (v_i - \bar{v_i})}{\sum_i^N \omega_i}
+
+    N = number of particles, \omega_i the weights.
 
 
-    for i in range(0,len(r_edges)-1):
-        wbin=np.where((r_ids>=r_edges[i]) & (r_ids<r_edges[i+1]))
+    Parameters:
+    ----------
+    pos : 3d numpy array
+        Array with the cartesian coordinates of the particles
+    vel : 3d numpy array
+        Array with the cartesian velocities of the particles
 
-        if(np.size(wbin)>0):
-            mass_bins[i]=np.sum(weights[wbin]) # sums the weights at each radial bin
-        else:
-            print('Warning: no particles shown in bin range [{:.2f}-{:.2f}] Kpc'.format(i, i+1))
-            mass_bins[i]=0.0
+    Returns:
+    --------
+    sigma_r : float
+        The value of sigma_r.
+    sigma_theta : float
+        The value of sigma_theta
+    sigma_phi : float
+        The value of sigma_phi
 
-    shells = np.ndarray(shape=np.size(r_edges), dtype=float)
-    #rint(mass_bins)
-    for i in range(0, np.size(r_edges)-1):
-        shells[i]=4/3.*np.pi*(r_edges[i+1]**3-r_edges[i]**3)
+    """
 
-    density = (mass_bins)/shells[:-1]
+    vr, v_theta, v_phi = vel_cartesian_to_spherical(pos, vel)
 
-    return density
+    vr1 = np.zeros(len(vr))
+    vtheta1 = np.zeros(len(vr))
+    vphi1 = np.zeros(len(vr))
+
+    for i in range(len(vr)):
+        vr1[i] = weights[i]*(vr[i]-np.mean(vr))**2
+        vtheta1[i] = weights[i]*(v_theta[i]-np.mean(v_theta))**2
+        vphi1[i] = weights[i]*(v_phi[i]-np.mean(v_phi))**2
+
+    sigma_r = np.sqrt(np.abs(np.sum(vr1))/np.sum(weights))
+    sigma_theta = np.sqrt(np.sum(vtheta1)/np.sum(weights))
+    sigma_phi = np.sqrt(np.sum(vphi1)/np.sum(weights))
+
+    return sigma_r, sigma_theta, sigma_phi
 
 
+def velocity_dispersions_r(pos, vel, n_bins, rmax, weights, weighted=0):
+    """
+    Compute the velocity dispersions in radial bins.i
+
+    Parameters:
+    ----------
+    pos : 3d numpy array
+        Array with the cartesian coordinates of the particles
+    vel : 3d numpy array
+        Array with the cartesian velocities of the particles
+    n_bins : int
+        Number of radial bins to compute the velocity dispersions.
+    rmax : int
+        Maximum radius to compute the velocity dispersions.
+
+    Returns:
+    --------
+    sigma_r : numpy array
+    sigma_theta : numpy array
+    sigma_phi : numpy array
+
+    """
+    dr = np.linspace(0, rmax, n_bins)
+    r = (pos[:,0]**2 + pos[:,1]**2 + pos[:,2]**2)**0.5
+    #r = pos
+    vr_disp_r = np.zeros(len(dr)-1)
+    vtheta_disp_r = np.zeros(len(dr)-1)
+    vphi_disp_r = np.zeros(len(dr)-1)
+
+    if weighted==1:
+        print('Computing the velocity dispersion profile for the stellar halo!')
+        for i in range(len(dr)-1):
+            index = np.where((r<dr[i+1]) & (r>dr[i]))
+            vr_disp_r[i], vtheta_disp_r[i], vphi_disp_r[i] = velocity_dispersion_weights(pos[index], vel[index]\
+                                                                                        , weights[index])
+
+    else:
+        for i in range(len(dr)-1):
+            index = np.where((r<dr[i+1]) & (r>dr[i]))
+            vr_disp_r[i], vtheta_disp_r[i], vphi_disp_r[i] = velocity_dispersion(pos[index], vel[index])
+
+    return vr_disp_r, vtheta_disp_r, vphi_disp_r
 
 if __name__ == "__main__":
 
